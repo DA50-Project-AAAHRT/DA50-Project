c98958527196bac8222b67feefc3da53408c227d
robsman67 <hakobyanrobert67@gmail.com>
2024-12-17 15:47:43 +0100

    Main view disconnection and new view (quizz, profile, wallet)

--- src/main/java/com/projet/da50/projet_da50/view/MainMenuView.java
+++ src/main/java/com/projet/da50/projet_da50/view/MainMenuView.java
@@ -3,6 +3,7 @@ package com.projet.da50.projet_da50.view;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -16,12 +17,43 @@ public class MainMenuView extends UI {
     public void show() {
 
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
+        grid.setAlignment(Pos.TOP_LEFT);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        //TODO
+
+        // Add Courses button
+        Button btnCourses = new Button("Courses");
+        btnCourses.setOnAction(e -> {
+            // Handle Courses button action
+        });
+        grid.add(btnCourses, 0, 0);
+
+        // Add Wallet button
+        Button btnWallet = new Button("Wallet");
+        btnWallet.setOnAction(e -> {
+            // Handle Wallet button action
+        });
+        grid.add(btnWallet, 0, 1);
+
+        // Add Disconnect button
+        Button btnDisconnect = new Button("Disconnect");
+        btnDisconnect.setOnAction(e -> {
+            // Redirect to AuthentificationFormView
+            new AuthentificationFormView(primaryStage).show();
+        });
+        grid.add(btnDisconnect, 0, 2);
+
+        // Add a My profil button
+        Button btnProfil = new Button("My profil");
+        btnProfil.setOnAction(e -> {
+            // Handle My profil button action
+        });
+        grid.add(btnProfil, 1, 2);
+
+        // Request focus on another element to prevent the Disconnect button from being selected
+        btnCourses.requestFocus();
 
         Scene scene = new Scene(grid, WINDOW_WIDTH, WINDOW_HEIGHT);
         primaryStage.setTitle("Main Menu");
