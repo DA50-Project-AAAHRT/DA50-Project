36c2f7779b44cc7a1b1824121da9abd51fe242ed
robsman67 <hakobyanrobert67@gmail.com>
2024-12-17 16:57:37 +0100

    Test begin

---
+++ src/test/java/UserControllerTest/UserControllerTest.java
@@ -0,0 +1,14 @@
+package UserControllerTest;
+import com.projet.da50.projet_da50.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class UserControllerTest {
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+}
