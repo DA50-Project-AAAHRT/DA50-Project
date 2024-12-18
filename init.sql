69940d8c6ce12acc86719b0adab1fd98514176b5
Thomas <thomas.pierron@utbm.fr>
2024-12-18 17:12:14 +0100

    Test fix github action file

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE da50;
-CREATE USER 'hibernate_user'@'%' IDENTIFIED BY 'root'; -- Remplace 'localhost' par '%'
-GRANT ALL PRIVILEGES ON da50.* TO 'hibernate_user'@'%'; -- Remplace 'localhost' par '%'
+CREATE USER 'hibernate_user'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON da50.* TO 'hibernate_user'@'%';
 FLUSH PRIVILEGES;
