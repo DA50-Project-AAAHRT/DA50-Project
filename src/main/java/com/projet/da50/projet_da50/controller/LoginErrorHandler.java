4a0dc4b4c6d2034e59471d175f933c3919226c9c
Thomas <thomas.pierron@utbm.fr>
2024-12-19 18:54:28 +0100

    Creating security on login attempts

--- src/main/java/com/projet/da50/projet_da50/controller/LoginErrorHandler.java
+++ src/main/java/com/projet/da50/projet_da50/controller/LoginErrorHandler.java
@@ -3,12 +3,20 @@ package com.projet.da50.projet_da50.controller;
 import com.projet.da50.projet_da50.model.User;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginErrorHandler {
 
     private UserController userController;
+    private Map<String, LoginAttempt> loginAttempts; // Map pour stocker les tentatives de connexion
+    private static final long LOCK_TIME_DURATION = 60 * 1000; // 30 secondes en millisecondes
 
-    public LoginErrorHandler(){
+    public LoginErrorHandler() {
         this.userController = new UserController();
+        this.loginAttempts = new HashMap<>();
     }
 
     public String validateAuthenticationFields(String username, String password) {
@@ -18,9 +26,24 @@ public class LoginErrorHandler {
 
         User user = userController.findUserByUsername(username);
 
-        if (user != null && BCrypt.checkpw(password, user.getPassword())) {
+        if (user == null) {
+            return "Invalid username or password.";
+        }
+
+        // Récupérer ou créer l'objet LoginAttempt pour cet utilisateur
+        LoginAttempt attempt = loginAttempts.computeIfAbsent(username, k -> new LoginAttempt());
+
+        if (attempt.isLocked(LOCK_TIME_DURATION)) {
+            long remainingTime = ChronoUnit.MILLIS.between(LocalDateTime.now(), attempt.getLastAttemptTime().plus(LOCK_TIME_DURATION, ChronoUnit.MILLIS));
+            return "Account locked. Try again in " + remainingTime / 1000 + " seconds.";
+        }
+
+        if (BCrypt.checkpw(password, user.getPassword())) {
+            attempt.resetAttempts();
+            loginAttempts.remove(username); // Supprimer l'entrée de la map après une connexion réussie
             return "Valid credentials.";
         } else {
+            attempt.incrementAttempts();
             return "Invalid username or password.";
         }
     }
